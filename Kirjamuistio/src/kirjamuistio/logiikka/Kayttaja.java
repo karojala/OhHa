6a91df50b0e19f8e0db426f174cb8f35409b57af
Karita E Ojala <karojala@dhp-17.cs.helsinki.fi>
2013-11-14 13:15:14 +0200

    Lisaa rajapintaa, Kayttaja

--- Kirjamuistio/src/kirjamuistio/logiikka/Kayttaja.java
+++ Kirjamuistio/src/kirjamuistio/logiikka/Kayttaja.java
@@ -1,40 +1,12 @@
 package kirjamuistio.logiikka;
 
-public class Kayttaja {
+public interface Kayttaja {
 
-    // Myöhemmin toteutetaan rajapinnan kautta
-    private String tunnus;
-    private String salasana;
+    public void setSalasana(String vanhaSalasana, String uusiSalasana);
 
-    public Kayttaja(String tunnus, String salasana) {
-        this.tunnus = tunnus;
-        this.salasana = salasana;
-    }
+    public boolean sallittuMerkkijono(String merkkijono);
 
-    public void setSalasana(String vanhaSalasana, String uusiSalasana) {
-        if (vanhaSalasana.equals(this.salasana) && sallittuMerkkijono(uusiSalasana) == true) {
-            this.salasana = uusiSalasana;
-        } else {
-            System.out.println("Syötetty vanha salasana on väärin tai salasana on alle 5 merkkiä tai yli 12 merkkiä.");
-        }
-    }
+    public String getSalasana();
 
-    public boolean sallittuMerkkijono(String merkkijono) {
-        boolean ok = true;
-
-        if (merkkijono.isEmpty() || merkkijono.length() < 5 || merkkijono.length() > 15) {
-            ok = false;
-        }
-
-        return ok;
-    }
-    
-    // Tämä toiminto myöhemmin mahdollista vain ylläpitäjälle (?) tai vastattava turvakysymykseen
-    public String getSalasana() {
-        return this.salasana;
-    }
-
-    public String getTunnus() {
-        return this.tunnus;
-    }
+    public String getTunnus();
 }
