f0ae5dee308461ccf2cf542da8b9e4c4405d5f65
Karita E Ojala <karojala@dhp-17.cs.helsinki.fi>
2013-11-14 13:08:27 +0200

    Kirjalista rajapinnaksi

---
+++ Kirjamuistio/src/kirjamuistio/logiikka/Halutut.java
@@ -0,0 +1,128 @@
+package kirjamuistio.logiikka;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Halutut implements Kirjalista {
+
+    // Kirjalista, haut jne. toteutetaan myöhemmin rajapinnan kautta
+    // CriteriaBuilder esim. ???
+    private HashMap<String, Kirja> kirjat;
+
+    public Halutut() {
+        this.kirjat = new HashMap<String, Kirja>();
+    }
+
+    @Override
+    public boolean tarkistaKirjanTiedot(Kirja kirja) {
+        boolean ok = true;
+
+        if (kirja.getNimi().isEmpty()) {
+            System.out.println("Kirjan nimi puuttuu.");
+            ok = false;
+        }
+
+        if (kirja.getKirjoittaja().isEmpty()) {
+            System.out.println("Kirjan kirjoittaja puuttuu.");
+            ok = false;
+        }
+
+        if (kirja.getJulkaisuvuosi() < 0 || kirja.getJulkaisuvuosi() > 2100) {
+            System.out.println("Kirjan julkaisuvuosi on virheellinen.");
+            ok = false;
+        }
+
+        return ok;
+    }
+
+    @Override
+    public void lisaaKirja(Kirja kirja) {
+        if (tarkistaKirjanTiedot(kirja) == false) {
+            return;
+        }
+
+        String nimisiisti = siistiMerkkijono(kirja.getNimi());
+
+        if (this.kirjat.containsKey(nimisiisti)) {
+            System.out.println("Kirja on jo listassa.");
+        } else {
+            kirjat.put(nimisiisti, kirja);
+        }
+    }
+
+    @Override
+    public void poistaKirja(String nimi) {
+        String nimisiisti = siistiMerkkijono(nimi);
+
+        if (this.kirjat.containsKey(nimisiisti)) {
+            this.kirjat.remove(nimisiisti);
+        } else {
+            System.out.println("Kirjaa ei ole listassa, joten sitä ei voida poistaa.");
+        }
+    }
+
+    @Override
+    // Myöhemmin toteutus, että voi hakea epätäydellisellä nimellä?
+    public String nimiHaku(String nimi) {
+        String nimisiisti = siistiMerkkijono(nimi);
+
+        return this.kirjat.get(nimisiisti).toString();
+    }
+
+    @Override
+    public String kirjoittajaHaku(String kirjoittaja) {
+        String kirjatMerkkijonona = "";
+
+        for (Kirja kirja : this.kirjat.values()) {
+            if (kirja.getKirjoittaja().equalsIgnoreCase(kirjoittaja)) {
+                kirjatMerkkijonona += kirja + "\n";
+            }
+        }
+
+        return kirjatMerkkijonona;
+    }
+
+    @Override
+    public String julkaisuvuosiHaku(int julkvuosi) {
+        String kirjatMerkkijonona = "";
+
+        for (Kirja kirja : this.kirjat.values()) {
+            if (kirja.getJulkaisuvuosi() == julkvuosi) {
+                kirjatMerkkijonona += kirja + "\n";
+            }
+        }
+
+        return kirjatMerkkijonona;
+    }
+
+    @Override
+    public boolean onkoKirja(Kirja kirja) {
+        return this.kirjat.containsValue(kirja);
+    }
+
+    @Override
+    public String siistiMerkkijono(String merkkijono) {
+        if (merkkijono == null) {
+            return "";
+        }
+
+        merkkijono = merkkijono.toLowerCase();
+        return merkkijono.trim();
+    }
+
+    @Override
+    public ArrayList<Kirja> kirjalista() {
+        return new ArrayList<Kirja>(this.kirjat.values());
+    }
+
+    @Override
+    public String toString() {
+        String kirjatMerkkijonona = "";
+
+        for (Kirja kirja : kirjalista()) {
+            kirjatMerkkijonona += "  " + kirja + "\n";
+        }
+
+        return "Käyttäjän haluamat kirjat: \n" + kirjatMerkkijonona;
+    }
+}
--- Kirjamuistio/src/kirjamuistio/logiikka/Main.java
+++ Kirjamuistio/src/kirjamuistio/logiikka/Main.java
@@ -4,17 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Tämä toteutetaan myöhemmin eri luokassa
-        Kirja kirja = new Kirja("The Selfish Gene", "Richard Dawkins", 1976, "Populaaritiede", "978-0-19-929115-1");
-        Kirja kirja2 = new Kirja("The Selfish Gene", "Richard Dawkins", 1976);
-        Kirja kirja3 = new Kirja("The God Delusion", "Richard Dawkins", 1980);
-        Kirja kirja4 = new Kirja("Statistical Methods For Psychology", "David C. Howell", 2012);
+        // Tämä toteutetaan myöhemmin käyttöliittymässä
+        Kirjalista omistetut = new Omistetut();
+        omistetut.lisaaKirja(new Kirja("The Selfish Gene", "Richard Dawkins", 1976, "Populaaritiede", "978-0-19-929115-1"));
+        omistetut.lisaaKirja(new Kirja("The Selfish Gene", "Richard Dawkins", 1976));
+        omistetut.lisaaKirja(new Kirja("The God Delusion", "Richard Dawkins", 1980));
+        omistetut.lisaaKirja(new Kirja("Statistical Methods For Psychology", "David C. Howell", 2012));
         
-        Kirjalista kirjalista = new Kirjalista();
-        kirjalista.lisaaKirja(kirja);
-        kirjalista.lisaaKirja(kirja2);
-        kirjalista.lisaaKirja(kirja3);
-        kirjalista.lisaaKirja(kirja4);
-        System.out.println(kirjalista);
+        Kirjalista halutut = new Halutut();
+        halutut.lisaaKirja(new Kirja("Abarat", "Clive Barker", 2000, "Nuorten fantasia"));
+        halutut.lisaaKirja(new Kirja("Pride and Prejudice", "Jane Austen", 1813, "Klassikot"));
+        
+        System.out.println(omistetut);
+        System.out.println(halutut);
     }
 }
